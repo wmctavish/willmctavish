9957bad8557f0786de08c44aa87caee78c867b0e
William McTavish <wbmctavish@gmail.com>
2019-04-17 14:00:45 -0400

    style work

--- main.js
+++ main.js
@@ -4,109 +4,73 @@ var content1tri = document.getElementById("traingle1");
 var content2tri = document.getElementById("traingle2");
 var content3tri = document.getElementById("traingle3");
 var content4tri = document.getElementById("traingle4");
+var content5tri = document.getElementById("traingle5");
 var content2 = document.getElementById("content2");
 var content3 = document.getElementById("content3");
 var content4 = document.getElementById("content4");
-
 var content5 = document.getElementById("content5");
-var content6 = document.getElementById("content6");
-var content7 = document.getElementById("content7");
-var content8 = document.getElementById("content8");
+
+
 
 document.getElementById("clickme1").addEventListener("click", () => {
     content1.classList.toggle("displayed");
     content1tri.classList.toggle("triangled");
-    if (content2.className || content3.className || content4.className == "contentrow1 displayed") {
+    if (content2.className || content3.className == "contentrow1 displayed") {
         content2.className = "contentrow1";
         content3.className = "contentrow1";
-        content4.className = "contentrow2";
     };
-    if (content2tri.className || content3tri.className || content4tri.className == "triangled") {
+    if (content2tri.className || content3tri.className == "triangled") {
         content2tri.className = "triangle";
         content3tri.className = "triangle";
-        content4tri.className = "triangle";
     };
-})
+});
 
 document.getElementById("clickme2").addEventListener("click", () => {
     content2.classList.toggle("displayed");
     content2tri.classList.toggle("triangled");
-    if (content1.className || content3.className || content4.className == "contentrow1 displayed") {
+    if (content1.className || content3.className == "contentrow1 displayed") {
         content1.className = "contentrow1";
         content3.className = "contentrow1";
-        content4.className = "contentrow2";
+        
     };
-    if (content1tri.className || content3tri.className || content4tri.className == "triangled") {
+    if (content1tri.className || content3tri.className == "triangled") {
         content1tri.className = "triangle";
         content3tri.className = "triangle";
-        content4tri.className = "triangle";
+        
     };
-})
+});
 
 document.getElementById("clickme3").addEventListener("click", () => {
     content3.classList.toggle("displayed");
     content3tri.classList.toggle("triangled");
-    if (content1.className || content2.className || content4.className == "contentrow1 displayed") {
+    if (content1.className || content2.className == "contentrow1 displayed") {
         content1.className = "contentrow1";
         content2.className = "contentrow1";
-        content4.className = "contentrow2";
     };
-    if (content1tri.className || content2tri.className || content4tri.className == "triangled") {
+    if (content1tri.className || content2tri.className == "triangled") {
         content1tri.className = "triangle";
         content2tri.className = "triangle";
-        content4tri.className = "triangle";
     };
-})
+});
 
 document.getElementById("clickme4").addEventListener("click", () => {
     content4.classList.toggle("displayed");
     content4tri.classList.toggle("triangled");
-    if (content1.className || content2.className || content3.className == "contentrow1 displayed") {
-        content1.className = "contentrow1";
-        content2.className = "contentrow1";
-        content3.className = "contentrow1";
+    if (content5.className == "contentrow2 displayed") {
+        content5.className = "contentrow2";
     };
-    if (content1tri.className || content2tri.className || content3tri.className == "triangled") {
-        content1tri.className = "triangle";
-        content2tri.className = "triangle";
-        content3tri.className = "triangle";
+    if (content5tri.className == "triangled") {
+        content5tri.classList.toggle("triangle");
     };
-})
-
-
+});
 
 document.getElementById("clickme5").addEventListener("click", () => {
     content5.classList.toggle("displayed");
-    if (content6.className || content6.className || content6.className == "contentrow2 displayed") {
-        content6.className = "contentrow2";
-        content7.className = "contentrow2";
-        content8.className = "contentrow2";
-    }
-})
-
-document.getElementById("clickme6").addEventListener("click", () => {
-    content6.classList.toggle("displayed");
-    if (content5.className || content7.className || content8.className == "content displayed") {
-        content5.className = "content";
-        content7.className = "content";
-        content8.className = "content";
-    }
-})
-
-document.getElementById("clickme7").addEventListener("click", () => {
-    content7.classList.toggle("displayed");
-    if (content5.className || content6.className || content8.className == "content displayed") {
-        content5.className = "content";
-        content6.className = "content";
-        content8.className = "content";
-    }
-})
-
-document.getElementById("clickme8").addEventListener("click", () => {
-    content8.classList.toggle("displayed");
-    if (content5.className || content6.className || content7.className == "content displayed") {
-        content5.className = "content";
-        content6.className = "content";
-        content7.className = "content";
-    }
-})
+    content5tri.classList.toggle("triangled");
+    if (content4.className == "contentrow2 displayed") {
+        content4.className = "contentrow2";
+    };
+    if (content4tri.className == "triangled") {
+        content4tri.classList.toggle("triangle");
+    };
+});
